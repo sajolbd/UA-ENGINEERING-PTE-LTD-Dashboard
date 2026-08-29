861588ecdcee53c1e6e9fbde19537fb61c6129c5
sajolbd <shamim15-9780@diu.edu.bd>
2026-08-29 08:03:43 +0600

    feat: configure live domain dashboard.uaengineering.com.sg and image remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'ua-engineering-pte-ltd-backend.vercel.app', port: '' },
+      { protocol: 'https', hostname: 'www.uaengineering.com.sg', port: '' },
+      { protocol: 'https', hostname: 'uaengineering.com.sg', port: '' },
+      { protocol: 'https', hostname: 'dashboard.uaengineering.com.sg', port: '' },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
